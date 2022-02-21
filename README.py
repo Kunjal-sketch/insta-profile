46e250a841b44719cd489a72b66767c2f37712c7
Kunjal-sketch <82381473+Kunjal-sketch@users.noreply.github.com>
2022-02-21 20:34:54 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,17 @@
+# insta-profile
+## Instgram profile information
+
+### install packages 
+```
+pip install -r requirements.txt
+```
+
+### Execute Program
+```
+python InstgramProfile.py <username>
+```
+
+### Sample run 
+
+
+<img src="output.png"/>
